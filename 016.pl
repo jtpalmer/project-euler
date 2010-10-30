c3214624512a39c7d15e55609662d6acfceb381b
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-30 13:36:20 -0400

    Problem 16, Perl 5.

---
+++ 016.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Math::BigInt;
+use List::Util qw(sum);
+
+my $two = Math::BigInt->new(2);
+my $exponent = Math::BigInt->new(1000);
+my $n = $two ** $exponent;
+
+say sum split('', $n);
