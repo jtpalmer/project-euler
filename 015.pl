6cd3beb5826882b9ee28cc0b9659fb66e6951237
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-30 13:33:15 -0400

    Problem 15, Perl 5.

---
+++ 015.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Memoize;
+
+memoize('routes');
+
+sub routes {
+    my ($x, $y) = @_;
+    return 2 if $x == 1 && $y == 1;
+    return $x + 1 if $y == 1;
+    return $y + 1 if $x == 1;
+    return routes($x - 1, $y) + routes($x, $y - 1);
+}
+
+say routes(20, 20);
