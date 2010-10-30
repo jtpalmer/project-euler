d3c694f015bd93f06f546d6161b89f001a9967cf
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-30 10:02:23 -0400

    Problem 7, Perl 5.

---
+++ 007.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Math::Prime::XS 'is_prime';
+
+my $n = 1;
+my $count = 0;
+
+while ($count < 10_001) {
+    $n++;
+    $count++ if is_prime($n);
+}
+say $n;
