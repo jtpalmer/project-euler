67f007b14c2716e435a53d162daf7714dcef29e5
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-29 20:33:55 -0400

    Problem 2, Perl 5.

---
+++ 002.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Memoize;
+
+memoize('fib');
+
+sub fib {
+    my ($n) = @_;
+    return $n if $n < 2;
+    return fib($n - 1) + fib($n - 2);
+}
+
+my $sum;
+my $n = 0;
+while ((my $f = fib($n)) <= 4_000_000) {
+    $n++;
+    next if $f % 2 == 1;
+    $sum += $f;
+}
+
+say $sum;
