2d76591b56814fafc2462f26e93f516543ed9dd6
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-30 15:39:14 -0400

    Problem 17, Perl 5.

---
+++ 017.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Lingua::EN::Numbers qw(num2en);
+
+my $letters;
+
+foreach my $n (1 .. 1000) {
+    my $number = num2en($n);
+    my @matches = ($number =~ /[a-z]/g);
+    $letters += @matches;
+}
+
+say $letters;
