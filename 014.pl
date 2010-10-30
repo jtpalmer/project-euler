89eb816fcc5e79252934a20d660833bdc9eb0576
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-30 12:59:13 -0400

    Problem 14, Perl 5.

---
+++ 014.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use Memoize;
+
+memoize('seq_length');
+
+sub collatz {
+    my ($n) = @_;
+    if ($n % 2 == 0) {
+        return $n / 2;
+    } else {
+        return 3 * $n + 1;
+    }
+}
+
+sub seq_length {
+    my ($n) = @_;
+    return 1 if $n == 1;
+    return 1 + seq_length(collatz($n));
+}
+
+my $max = 0;
+my $n;
+foreach my $x (1 .. 999_999) {
+    my $length = seq_length($x);
+    if ($length > $max) {
+        $max = $length;
+        $n = $x;
+    }
+}
+say $n;
