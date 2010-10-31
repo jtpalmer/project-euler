061c6303bcfa89f7dc05725719d6368254407259
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-10-31 12:37:39 -0400

    Problem 22, Perl 5.

---
+++ 022.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use List::Util qw(sum);
+use Text::CSV;
+
+sub alpha_value {
+    my ($letter) = @_;
+    return ord($letter) - 64;
+}
+
+sub score {
+    my ( $position, $name ) = @_;
+    return $position * sum map { alpha_value($_) } split( //, $name );
+}
+
+my $csv = Text::CSV->new();
+open my $fh, '<', 'names.txt' or die $!;
+my @names = @{ $csv->getline($fh) };
+
+@names = sort @names;
+
+say sum map { score( $_ + 1, $names[$_]) } 0 .. $#names;
