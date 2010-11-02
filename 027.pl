76cae85c7a2addc3050173a569cef68a3774908e
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-11-01 21:22:18 -0400

    Optimize problem 27.

--- 027.pl
+++ 027.pl
@@ -3,26 +3,37 @@ use strict;
 use warnings;
 use 5.010;
 use Math::Prime::XS qw(is_prime);
+use List::MoreUtils qw(any);
 
-sub f {
-    my ($a, $b, $n) = @_;
-
-    return $n * $n  + $a * $n + $b;
-}
+my %prime;
+my %not_prime;
 
 sub _is_prime {
-    return unless $_[0] > 1;
-    return is_prime($_[0]);
+    my ($n) = @_;
+
+    return unless $n > 1;
+
+    return   if exists $not_prime{$n};
+    return 1 if exists $prime{$n};
+
+    if ( is_prime($n) ) {
+        $prime{$n} = 1;
+        return 1;
+    }
+    else {
+        $not_prime{$n} = 1;
+        return;
+    }
 }
 
 my $max = 0;
 my $product;
-for my $a (-999 .. 999) {
-    for my $b (-999 .. 999) {
+for my $a ( -999 .. 999 ) {
+    for my $b ( -999 .. 999 ) {
         my $n = 0;
-        while (_is_prime(f($a, $b, $n))) { $n++ }
-        if ($n > $max) {
-            $max = $n;
+        while ( _is_prime( $n * $n + $a * $n + $b ) ) { $n++ }
+        if ( $n > $max ) {
+            $max     = $n;
             $product = $a * $b;
         }
     }
